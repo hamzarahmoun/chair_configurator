4597458f24aa3728f08a9a5d9c31ccf315232402
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-24 17:42:31 +0100

    box in the middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+import './index.css'
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-
   return (
-   <div>
-    Hamza
-   </div>
-  )
+    <>
+    
+      <Canvas  camera={{ position: [1, 1, 3], fov: 50 }} shadows>
+      <OrbitControls />
+        <Experience />
+      </Canvas>
+    </>
+  );
 }
 
-export default App
+export default App;
