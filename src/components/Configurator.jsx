458e7c98e4f8368314837d33c63726f854d8efd3
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-30 17:39:29 +0100

    add chaire configurator variable and css

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -1,8 +1,18 @@
 const Configurator = () => {    
   return (
     <div className="configurator">
-      Hello
+  <div className="configurator_section">
+    <div className="configurator_section_title">Chair material</div>
+    <div className="configurator_section_values">
+      <div className="item">
+        <div className="item_label">Leather</div>
+      </div>
+      <div className="item">
+        <div className="item_label">Fabric</div>
+      </div>
     </div>
+  </div>
+</div>
   );
 }
 export default Configurator;
