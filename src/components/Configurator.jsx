72057c36c683eecaf20c6bf587cb6bc51fa312a1
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-30 17:59:47 +0100

    switch selection between var

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { useCustomization } from "../contexts/Customization";
 
-const Configurator = () => { 
-    const {    material, setMaterial } = useCustomization();   
+const Configurator = () => {
+    const { material, setMaterial } = useCustomization();
     console.log(material);
-  return (
-    <div className="configurator">
-  <div className="configurator_section">
-    <div className="configurator_section_title">Chair material</div>
-    <div className="configurator_section_values">
-      <div className="item" onClick={() => setMaterial("leather")}
-      >
-        <div className="item_label">Leather</div>
-      </div>
-      <div className="item" onClick={() => setMaterial("fabric")}>
-        <div className="item_label">Fabric</div>
-      </div>
-    </div>
-  </div>
-</div>
-  );
+    return (
+        <div className="configurator">
+            <div className="configurator_section">
+                <div className="configurator_section_title">Chair material</div>
+                <div className="configurator_section_values">
+                    <div className={`item ${material === "leather" ? "item--active" : ""}`}
+
+                        onClick={() => setMaterial("leather")}
+                    >
+                        <div className="item_label">Leather</div>
+                    </div>
+                    <div className={`item ${material === "fabric" ? "item--active" : ""}`}
+                        onClick={() => setMaterial("fabric")}>
+                        <div className="item_label">Fabric</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Configurator;
